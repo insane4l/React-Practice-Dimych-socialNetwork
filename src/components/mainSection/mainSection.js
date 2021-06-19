c55db2162a1128470a8b9e65aa5cf76eaa2f74b5
insane4l <43768644+insane4l@users.noreply.github.com>
2021-06-19 12:10:22 +0300

    Practice from 50 - 54 lessons (REST API, users API, side effect, class component, lifecycle)

--- src/components/mainSection/mainSection.js
+++ src/components/mainSection/mainSection.js
@@ -9,12 +9,8 @@ const MainSection = () => {
         <main className="main-section">
             <div className="section">
                 <Route path="/profile" component={ProfilePage} />
-<<<<<<< HEAD
-                <Route path="/messages"  component={MessagesPage} />
-=======
                 <Route path="/friends" component={FriendsPage} />
                 <Route path="/messages" component={MessagesPage} />
->>>>>>> Friends page created and stylized (without links)
                 <Route path="/news" component={NewsPage} />
                 <Route path="/music" component={MusicPage} />
                 <Route path="/settings" component={SettingsPage} />
