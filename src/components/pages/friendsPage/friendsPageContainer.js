512fc37f935e675adf2226e765cfc6d73e9d886a
insane4l <43768644+insane4l@users.noreply.github.com>
2021-06-22 22:15:01 +0300

    Spinner created, when fetching users. Practice from 57 lesson.

--- src/components/pages/friendsPage/friendsPageContainer.js
+++ src/components/pages/friendsPage/friendsPageContainer.js
@@ -2,38 +2,48 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import * as axios from 'axios';
 import FriendsPage from './friendsPage';
-import {toggleFollowedAC, setUsersAC, setPageNumber, setTotalUsersCount} from '../../../reducers/friendsPageReducer';
+import Spinner from '../../spinner';
+import {toggleFollowedAC, setUsersAC, setPageNumber, setTotalUsersCount, setIsLoading} from '../../../reducers/friendsPageReducer';
 
 
 
 class FriendsPageContainer extends Component {
 
     componentDidMount() {
+        this.props.setIsLoading(true);
         const _apiBase = 'https://social-network.samuraijs.com/api/1.0';
         axios.get(`${_apiBase}/users?count=${this.props.pageSize}&page=${this.props.currentPage}`)
         .then(response => {
+            this.props.setIsLoading(false); 
             this.props.setUsers(response.data.items);
-            this.props.setTotalUsersCount(response.data.totalCount);   
+            this.props.setTotalUsersCount(response.data.totalCount);  
         });
     }
 
     onPageSelected = (num) => {
+        this.props.setIsLoading(true);
         this.props.setPageNumber(num);
         const _apiBase = 'https://social-network.samuraijs.com/api/1.0';
             axios.get(`${_apiBase}/users?count=${this.props.pageSize}&page=${num}`)
             .then(response => {
-                this.props.setUsers(response.data.items); 
+                this.props.setIsLoading(false);
+                this.props.setUsers(response.data.items);
             });
     }
 
     render () {
-        return <FriendsPage 
-                    totalUsersCount={this.props.totalUsersCount}
-                    pageSize={this.props.pageSize}
-                    currentPage={this.props.currentPage}
-                    onPageSelected={this.onPageSelected}
-                    users={this.props.users}
-                    toggleFollowed={this.props.toggleFollowed}/>
+        return (
+            <>
+                {this.props.isLoading ? <Spinner/> : null}
+                <FriendsPage 
+                            totalUsersCount={this.props.totalUsersCount}
+                            pageSize={this.props.pageSize}
+                            currentPage={this.props.currentPage}
+                            onPageSelected={this.onPageSelected}
+                            users={this.props.users}
+                            toggleFollowed={this.props.toggleFollowed}/>
+            </>
+        )
     }
 }
 
@@ -42,7 +52,8 @@ const mapStateToProps = (state) => {
         users: state.friendsPage.users,
         totalUsersCount: state.friendsPage.totalUsersCount,
         pageSize: state.friendsPage.pageSize,
-        currentPage: state.friendsPage.currentPage 
+        currentPage: state.friendsPage.currentPage,
+        isLoading: state.friendsPage.isLoading 
     }
 };
 
@@ -60,6 +71,9 @@ const mapDispatchToProps = (dispatch) => {
         },
         setPageNumber: (num) => {
             dispatch(setPageNumber(num));
+        },
+        setIsLoading: (loading) => {
+            dispatch(setIsLoading(loading));
         }
     }
 };
