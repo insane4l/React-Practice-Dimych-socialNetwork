82135c0e3e205bf3f830880a839efc54a47cc6c6
insane4l <43768644+insane4l@users.noreply.github.com>
2021-07-14 09:44:02 +0300

    Tests, jest, TDD. Practice from 89 lesson

---
+++ src/reducers/profileReducer.test.js
@@ -0,0 +1,47 @@
+import profilePageReducer, {addNewPost} from './profilePageReducer';
+
+const state = {
+    messages: [
+        {id: 1, label: 'Hi, this is my 1 post', likesCount: 22},
+        {id: 2, label: '2 post', likesCount: 10},
+        {id: 3, label: 'ok this is 3 post', likesCount: 20}
+    ]
+};
+
+it('Posts count must be incremented by 1', () => {
+    // 1. input data
+    const action = addNewPost(111)
+
+    // 2. action
+    const newState = profilePageReducer(state, action);
+
+    // expectation
+    expect(newState.messages.length).toBe(4);
+});
+
+
+
+it('New post likes count must be 0', () => {
+    // 1. input data
+    const action = addNewPost(111)
+
+    // 2. action
+    const newState = profilePageReducer(state, action);
+
+    // expectation
+    expect(newState.messages[3].likesCount).toBe(0);
+})
+
+
+
+// programming practice TDD
+it('Posts must be decremented by 1', () => {
+    // 1. input data
+    const action = deletePost(1);
+
+    // 2. action
+    const newState = profilePageReducer(state, action);
+
+    // expectation
+    expect(newState.messages.length).toBe(2);
+})
