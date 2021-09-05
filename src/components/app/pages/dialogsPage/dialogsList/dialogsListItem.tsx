36f3f5797cb24e973673ed050844b7a647a2d475
insane4l <43768644+insane4l@users.noreply.github.com>
2021-09-05 14:42:01 +0300

    React-moment lib added. Dialogs List dates parsed

--- src/components/app/pages/dialogsPage/dialogsList/dialogsListItem.tsx
+++ src/components/app/pages/dialogsPage/dialogsList/dialogsListItem.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { defaultPhoto } from '../../../../../assets/images'
 import { AllDialogsListItemType } from '../../../../../services/dialogsAPI'
+import Moment from 'react-moment'
 
 const DialogsListItem: React.FC<PropsType> = ({dialogsItem}) => {
 
@@ -19,14 +20,16 @@ const DialogsListItem: React.FC<PropsType> = ({dialogsItem}) => {
                     <div className="dialog__content-title">
                         {userName}
                         <span className="dialog__content-subtitle">
-                            {` (last seen ${lastUserActivityDate})`}
+                        &#160;(last seen&#160;
+                            <Moment fromNow date={lastUserActivityDate} />
+                            )
                         </span>
                     </div>
                     
                     <div className="dialog__content-message">
                         {hasNewMessages 
                             ? `There are ${newMessagesCount} new messages` 
-                            : `Last message on ${lastDialogActivityDate}`}
+                            : <span>Last message on&#160;<Moment format="MMM DD YYYY hh:mm" date={lastDialogActivityDate} /></span>}
                     </div>
                 </div>
             </Link>
